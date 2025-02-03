820227d68b42c7998c3ead8fb587a4b9eb360844
Rana Raad <202210376@ua.edu.lb>
2025-02-03 22:28:47 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './navbar';
-import HomePage from './home';
-import SearchPage from './search';
-import PostListingForm from './form';
-import AboutContact from './contact';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./navbar";
+import HomePage from "./home";
+import SearchPage from "./search";
+import PostListingForm from "./form";
+import AboutContact from "./contact";
+import "./index.css";
 
 const App = () => {
-
   return (
-    <Router>
+    <Router basename="/rentahome">
       <div>
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/search" element={<SearchPage/>} />
+          <Route path="/search" element={<SearchPage />} />
           <Route path="/post-listing" element={<PostListingForm />} />
-          <Route path="/about-contact" element={<AboutContact/>} />
+          <Route path="/about-contact" element={<AboutContact />} />
         </Routes>
       </div>
     </Router>
